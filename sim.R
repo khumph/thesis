34f002ff699803f0a17325d42f4e7387bc110e5b
kylehumphrey <kylehumphrey9@gmail.com>
2016-10-07 08:06:24 -0700

    bound toxicity at 0, make mass and tox after death NA, migrate simulation code to sim.R

--- sim.R
+++ sim.R
@@ -1,44 +1,28 @@
-library(tidyverse)
+library(pacman)
+p_load(tidyverse, rms)
 
-# simulate 100 patients
+# 1000 patients
 N <- 1000
+# 6 months treatment
+Ttot <- 6 
 
-# over six months of treatment
-Ttot <- 6
-
-# initialize toxicities, doses
+# choose random initial toxicities and tumor masses
 set.seed(1)
 W0 <- runif(N, min = 0, max = 2)
-set.seed(1)
-D0 <- runif(N, min = 0.5, max = 1)
+M0 <- runif(N, min = 0, max = 2)
 
-set.seed(1)
+# choose random starting dose > 0.5
+D0 <- runif(N, min = 0.5, max = 1)
+# choose random subsequent doses between 0 and 1 (max tolerable)
 D <- replicate(5, runif(N, min = 0, max = 1))
 D <- cbind(D0, D)
 
-data_frame(
-  id = 1:1000,
-  toxicity = W0,
-  dose = D0,
-  month = 0
-)
-
-# define functions for how tumor mass & toxicity change as a result of dose, each other
 Wdot <- function(M, D, a1 = 0.1, b1 = 1.2, d1 = 0.5) { 
   a1 * M + b1 * (D - d1)
 }
 
 Mdot <- function(M, W, D, a2 = 0.15, b2 = 1.2, d2 = 0.5) { 
   (a2 * W - b2 * (D - d2)) * ifelse(M > 0, 1, 0)
-}
-
-W <- matrix(c(W0, numeric(6000)), ncol = 7)
-M <- matrix(c(M0, numeric(6000)), ncol = 7)
-
-for (i in 1:(Ttot)) {
-  M_next <- ifelse(M[, i] == 0, 0, Wdot(M[, i], D[, i]) + W[, i])
-  W[, i + 1] <- Wdot(M[, i], D[, i]) + W[, i]
-  M[, i + 1] <- ifelse(M_next <= 0, 0, M_next)
 }
 
 R2 <- function(W1, W0) {
@@ -52,68 +36,55 @@ R3 <- function(M1, M0) {
                 ifelse(M1 - M0 <= -0.5, 5,
                        ifelse(M1 - M0 >= 0.5, -5, 0)))
   )
-  
 }
 
-r2 <- matrix(c(numeric(6000)), ncol = 6)
-r3 <- matrix(c(numeric(6000)), ncol = 6)
-for (i in 1:(Ttot)) {
-  r2[, i] <- R2(W[, i + 1], W[, i])
-  r3[, i] <- R3(M[, i + 1], M[, i])
-}
-
-
-lambda <- function(W, M, mu0, mu1, mu2) {
+lambda <- function(W, M, mu0 = -7, mu1 = 1, mu2 = 1) {
   exp(mu0 + mu1 * W + mu2 * M)
 }
 
-mu0 <- -7 ### MADE UP. NO VALUE GIVEN IN TEXT
-mu1 <- 1
-mu2 <- 1
+r <- matrix(c(numeric(6000)), ncol = 6)
+died <- matrix(c(numeric(6000)), ncol = 6)
+W <- matrix(c(W0, numeric(6000)), ncol = 7)
+M <- matrix(c(M0, numeric(6000)), ncol = 7)
 
-
-lams <- matrix(numeric(6000), ncol = 6)
 for (i in 1:(Ttot)) {
-  lams[, i] <- lambda(W[, i + 1], M[, i + 1], mu0, mu1, mu2)
-}
-deltaF <- exp(-lams)
-
-
-p <- 1 - deltaF
-
-set.seed(1)
-died <- apply(p, c(1, 2), function(p) rbinom(1, 1, p)) 
-r1 <- ifelse(died == 1, -60, 0)
-
-r <- r1 + r2 + r3
-
-zeroRewards <- function(x, died = died, i) { 
-  # zeroR function zeros rewards after death of a patient
-  # inputs:
-  #   matrix of total rewards (r) 
-  #   matrix of deaths (died)
-  #   an index of a particular patient of interest (i)
-  # returns:
-  #   row vector of rewards for the patient with rewards following death = 0
-  if (sum(died[i, ]) >= 1) {
-    first <- which(died[i, ] == 1)[1]
-    if (first < ncol(died)) {x[i, (first + 1):ncol(died)] <- 0} 
-    return(x[i, ])
-  } 
-  return(x[i, ])
-}
-
-for (i in 1:nrow(r)) {
-  r[i, ] <- zeroRewards(r, died, i)
+  for (j in 1:N) {
+    if (sum(died[j, 1:i]) == 0) {
+      # if patient cured (tumor mass == 0), mass stays 0
+      M_next <- ifelse(M[j, i] == 0, 0, Mdot(M[j, i], W[j, i], D[j, i]) + M[j, i])
+      W_next <- Wdot(M[j, i], D[j, i]) + W[j, i]
+      # mass and toxicity bounded at 0
+      W[j, i + 1] <- ifelse(W_next <= 0, 0, W_next)
+      M[j, i + 1] <- ifelse(M_next <= 0, 0, M_next)
+      # determine if patient died
+      lam <- lambda(W[j, i + 1], M[j, i + 1])
+      deltaF <- exp(-lam)
+      p <- 1 - deltaF
+      died[j, i] <- rbinom(1, 1, p)
+      # add up rewards
+      r[j, i] <-
+        R2(W[j, i + 1], W[j, i]) +
+        R3(M[j, i + 1], M[j, i]) +
+        ifelse(died[j, i] == 1, -60, 0)
+    } else {
+      # if patient already died, can't die again or get rewards, mass and tox to NA
+      r[j, i] <- 0
+      if (i != Ttot) {
+        died[j, i + 1] <- 0
+      } else {died[j, i] <- 0}
+      M[j, i + 1] <- NA
+      W[j, i + 1] <- NA
+    }
+  }
 }
 
-
-colnames(D) <- c(0:5)
-colnames(M) <- c(0:6)
-colnames(W) <- c(0:6)
-colnames(r) <- c(0:5)
-dat <- data.frame(D = D, M = M, W = W, r = r) %>% tbl_df() %>%
-  rownames_to_column(var = "ID") %>% mutate(ID = factor(ID))
+colnames(D) <- 0:5
+colnames(M) <- 0:6
+colnames(W) <- 0:6
+colnames(r) <- 0:5
+colnames(died) <- 0:5
+dat <- data.frame(D = D, M = M, W = W, r = r, d = died) %>% tbl_df() %>% 
+  rownames_to_column(var = "ID")
 
 dat_long <- dat %>%
   gather(key, value, -ID) %>%
@@ -127,5 +98,7 @@ dat_long <- dat %>%
     dose = D,
     tumor_mass = M,
     toxicity = W,
-    reward = r
+    reward = r,
+    died = d
   )
+# View(dat_long)
