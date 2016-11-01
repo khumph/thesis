9c9994a7008f94a6a7cbf557442fad86ddcd74ca
kylehumphrey <kylehumphrey9@gmail.com>
2016-11-01 07:27:18 -0700

    Test out using optimize function to optimize

---
+++ optim.R
@@ -0,0 +1,25 @@
+
+dose <- numeric(200)
+reward <- numeric(200)
+for (i in 1:200) {
+  f <- function(x) {
+    predict(mod, data.frame(dat, dose = x)[i, ])
+  }
+  opt <- optimize(f, interval = c(0,1), tol = 0.01, maximum = T)
+  dose[i] <- opt$maximum
+  reward[i] <- opt$objective
+}
+
+f
+
+x <- seq(0, 1, by = 0.01)
+dat %>%
+  mutate(dose = map(1:nrow(.), ~ x)) %>%
+  unnest() %>%
+  mutate(preds = predict(mod, .)) %>%
+  group_by(ID) %>%
+  mutate(max = max(preds),
+         best = ifelse(is.na(max),
+                       NA,
+                       which.max(preds) - 1) / 100) %>%
+  select(-dose, -preds) %>% unique()
