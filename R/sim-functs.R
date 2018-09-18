66fb049e8fb5c45367967f72fc3e2fa85a2dc6a5
khumph <kylehumphrey9@gmail.com>
2018-09-18 07:45:02 -0700

    Create W and M next functions for greater portability

--- R/sim-functs.R
+++ R/sim-functs.R
@@ -1,11 +1,23 @@
 updateW <- function(M, W, D, a = 0.1, b = 1.2, c, d = 0.5) {
   W_next <- a * M + b * (c * D  - d) + W
-  replace(W_next, W_next < 0, 0)
+  return(replace(W_next, W_next < 0, 0))
+}
+
+Wnext <- function(dat) {
+  dat$W_next <- updateW(dat$tumor_mass, dat$toxicity, dat$dose, c = dat$cW)
+  dat$W_next <- replace(dat$W_next, dat$dead, NA_real_)
+  return(dat)
 }
 
 updateM <- function(M, W, D, a = 0.15, b = 1.2, c, d = 0.5, Z = 0, g = 1e-3) {
   M_next <- a * W - b * (c * D - d) + M + sum(g * Z)
-  replace(M_next, M <= 0 | M_next < 0, 0)
+  return(replace(M_next, M <= 0 | M_next < 0, 0))
+}
+
+Mnext <- function(dat) {
+  dat$M_next <- updateM(dat$tumor_mass, dat$toxicity, dat$dose, c = dat$cM)
+  dat$M_next <- replace(dat$M_next, dat$dead, NA_real_)
+  return(dat)
 }
 
 genIntNoise <- function(dat, int, noise, noise_pred) {
@@ -50,10 +62,8 @@ lambda <- function(M, W, Z, mu0 = -5.5, mu1 = 1, mu2 = 1.2, mu3 = 0.75, a3 = 0.0
 
 
 simMonth <- function(dat) {
-  dat$W_next <- updateW(dat$tumor_mass, dat$toxicity, dat$dose, c = dat$cW)
-  dat$M_next <- updateM(dat$tumor_mass, dat$toxicity, dat$dose, c = dat$cM)
-  dat$W_next <- replace(dat$W_next, dat$dead, NA_real_)
-  dat$M_next <- replace(dat$M_next, dat$dead, NA_real_)
+  dat <- Mnext(dat)
+  dat <- Wnext(dat)
   dat$lambda <- lambda(dat$M_next, dat$W_next, Z = dat$noise_chng)
   dat$d_next <- runif(nrow(dat), min = 0, max = 1)
   dat$surv_time <- rexp(nrow(dat), dat$lambda)
